40f09b2b6f7a80418005edce910b3aa590301238
Fachri Misykatul Anwar <fachriandroid@gmail.com>
2023-11-09 00:29:17 +0700

    Navigasi

--- boostrap.js
+++ boostrap.js
@@ -1,24 +1,22 @@
+
 // Fungsi untuk menampilkan elemen dengan id yang diberikan dan menyembunyikan yang lainnya
 function showContent(id) {
 
-    const divs = document.querySelectorAll('container');
+    const divs = document.querySelectorAll('.container');
     divs.forEach(div => {
         div.style.display = "none";
     });
-    console.log(id);
-    const selectedDiv = document.getElementById("Home");
+    const selectedDiv = document.getElementById(id);
     selectedDiv.style.display = "block";
 }
 
 // Tambahkan event listener ke setiap tombol navigasi
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll('.navigasi');
 navLinks.forEach(link => {
     link.addEventListener('click', (event) => {
+        console.log("Clicked!");
         event.preventDefault(); // Mencegah halaman berpindah ke URL
         const id = link.id.replace('-button', ''); // Mengambil id div yang sesuai
         showContent(id);
     });
 });
-
-// Tampilkan halaman Home saat halaman pertama kali dimuat
-showContent("Home");
